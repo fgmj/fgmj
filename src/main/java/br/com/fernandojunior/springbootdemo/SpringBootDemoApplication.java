f6eb4367b2282537c52e0de0911c872c957a3c85
fernando.medeiros <fernando.medeiros@gmail.com>
2023-07-10 21:29:06 -0300

    Versão inicial.

---
+++ src/main/java/br/com/fernandojunior/springbootdemo/SpringBootDemoApplication.java
@@ -0,0 +1,13 @@
+package br.com.fernandojunior.springbootdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDemoApplication.class, args);
+	}
+
+}
